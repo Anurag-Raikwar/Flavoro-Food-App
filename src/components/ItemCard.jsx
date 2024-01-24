281ed17acb1cfde6c0c182f9dd4e0c001cf8f16a
Anurag Raikwar <raikwarannu2207@gmail.com>
2024-01-24 22:03:38 +0530

    Flavoro Food App

---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { removeFromCart,incrementQuantity,decrementQuantity } from "../redux/slices/CartSlice";
+import toast,{Toaster} from "react-hot-toast"
+
+const ItemCard = ({ id, name, price, qty, img }) => {
+  const dispatch = useDispatch();
+  const removeItem = ()=>{
+    toast(`${name} Removed!`,{
+      icon: "👋"
+    })
+  }
+  return (
+    <>
+      <div className="flex gap-2 shadow-md rounded-lg p-2 mb-3">
+        <MdDelete
+          onClick={() =>{
+            dispatch(removeFromCart({ id, img, name, price, qty }));
+            removeItem();}
+          }
+          className="absolute right-7 text-gray-600 cursor-pointer"
+        />
+        <img src={img} alt="" className="w-[50px] h-[50px]" />
+        <div className="leading-5">
+          <h2 className="font-bold text-gray-800">{name}</h2>
+          <div className="flex justify-between">
+            <span className="text-green-500 font-bold">₹{price}</span>
+            <div className="flex justify-center items-center gap-2 absolute right-7">
+              <AiOutlineMinus onClick={()=> qty>1 ? dispatch(decrementQuantity({id})): qty=0} className="border-2 border-gray-600 text-gray-600 rounded-md hover:bg-green-500 hover:text-white hover:border-none text-xl p-1 transition-all ease-linear cursor-pointer" />
+              <span>{qty}</span>
+              <AiOutlinePlus onClick={()=>dispatch(incrementQuantity({id}))} className="border-2 border-gray-600 text-gray-600 rounded-md hover:bg-green-500 hover:text-white hover:border-none text-xl p-1 transition-all ease-linear cursor-pointer" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ItemCard;
